3fdedb49ba468f4ec81a433384a16fa43b118864
Zied <zied.kannou15@gmail.com>
2026-02-04 18:07:28 +0100

    Initial commit: Spring Boot project setup

---
+++ user-role-management-backend/src/main/java/com/userrole/user_role_management_backend/UserRoleManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.userrole.user_role_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRoleManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRoleManagementBackendApplication.class, args);
+	}
+
+}
---
+++ user-role-management-backend/src/test/java/com/userrole/user_role_management_backend/UserRoleManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userrole.user_role_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRoleManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
