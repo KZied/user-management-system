78b26bb7298a28195f5afa62174dfb280acc3e76
Zied <zied.kannou15@gmail.com>
2026-02-04 19:00:28 +0100

    backend: add User and Role repositories

---
+++ user-role-management-backend/src/main/java/com/userrole/user_role_management_backend/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.userrole.user_role_management_backend.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleType name);
+}
---
+++ user-role-management-backend/src/main/java/com/userrole/user_role_management_backend/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.userrole.user_role_management_backend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByEmail(String email);
+}
